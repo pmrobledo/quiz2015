f43b95f02c89960c53d132601569eab4786bc83d
pmrobledo <martin.pedro@outlook.com>
2015-07-13 20:05:11 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ router.get('/', function(req, res) {
 });
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-router.get('/autor', quizController.autor);
+router.get('/author', quizController.autor);
 
 module.exports = router;
